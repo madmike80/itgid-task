b5927666f7c88bdfe109a12dd76fb580f61f2234
Mihail Treschalin <mihail.treschalin@ya.ru>
2021-10-12 01:16:56 +0300

    The tasks are solved

--- unit_10/unit_10.js
+++ unit_10/unit_10.js
@@ -1,10 +1,11 @@
 // Task 1
 //Создайте массив ar1 содержащий строки, числа, булевы значения. Выведите его в .out-1. Вывод - по нажатию кнопки b-1
 
-// let ar1 = // переменную обьявляем здесь!!!!
+let ar1 = ['qwerty', 4.25, 8, 23, true, 'asdf', false];
+let out1 = document.querySelector('.out-1');
 
 function f1() {
-    //ваш_элемент.innerHTML = ar1; // да так можно выводить!!!!
+  out1.innerHTML = ar1; // да так можно выводить!!!!
 }
 
 document.querySelector('.b-1').onclick = f1;
@@ -12,14 +13,14 @@ document.querySelector('.b-1').onclick = f1;
 // Task 2
 //Создайте массив ar2 содержащий строки, числа, булевы значения. Выведите его в div.out-2. Используйте шаблон вывода из кода в JS. Вывод - по нажатию кнопки b-2
 
-// let ar2 = // переменную обьявляем здесь!!!!
+let ar2 = [42, 123, 'яблоко', true, 'банан'];
 
 function f2() {
-    // let out = '';
-    // for (let i = 0; i < ar2.length; i++) {
-    //     out +=arr2[i]+' ';
-    // }
-    // document.querySelector('.out-2').innerHTML = out;
+  let out = '';
+  for (let i = 0; i < ar2.length; i++) {
+    out += ar2[i] + ' ';
+  }
+  document.querySelector('.out-2').innerHTML = out;
 }
 
 document.querySelector('.b-2').onclick = f2;
@@ -30,9 +31,11 @@ document.querySelector('.b-2').onclick = f2;
 // Вывод в out-3
 // Тест допустим массив [1,2,3] вывод - 3
 
-// let ar3 =  // переменную обьявляем здесь!!!!
+let ar3 = ['banana', 'orange', 'apple', 'mango'];
+let out3 = document.querySelector('.out-3');
 
 function f3() {
+  out3.innerHTML = ar3.length;
 }
 
 document.querySelector('.b-3').onclick = f3;
@@ -44,9 +47,14 @@ document.querySelector('.b-3').onclick = f3;
 // Вывод в out-4
 // Тест допустим массив [1,2,3,4,5,6,7,8,9,10] вывод: 1 4 9
 
-// let ar4 =  // переменную обьявляем эту здесь!!!!
+let ar4 = [45, 98, 32, 65, 72, 3, 41, 8, 12, 37, 29];
+let out4 = document.querySelector('.out-4');
 
 function f4() {
+  let out = '';
+  out += ar4[0] + ' ' + ar4[3] + ' ' + ar4[8];
+
+  out4.innerHTML = out;
 }
 
 document.querySelector('.b-4').onclick = f4;
@@ -58,9 +66,12 @@ document.querySelector('.b-4').onclick = f4;
 // Вывод в out-5
 // Тест допустим массив [1,2,3,4,5,6,7,8,9,10] вывод: 8
 
-// let ar5 =
+let ar5 = [45, 98, 32, 65, 72, 3, 41, 8, 12, 37, 29];
+let out5 = document.querySelector('.out-5');
 
 function f5() {
+  sum = ar5[0] + ar5[2] + ar5[3];
+  out5.innerHTML = sum;
 }
 
 document.querySelector('.b-5').onclick = f5;
@@ -71,9 +82,17 @@ document.querySelector('.b-5').onclick = f5;
 // Вывод - по нажатию кнопки b-6
 // Вывод в out-6
 
-// let ar6 =
+let ar6 = ['михаил', 'лев', 30, 'июль'];
+let out6 = document.querySelector('.out-6');
 
 function f6() {
+  let out = '';
+
+  for (let i = 0; i <= ar6.length - 1; i++) {
+    out += ar6[i] + ' ';
+  }
+
+  out6.innerHTML = out;
 }
 
 document.querySelector('.b-6').onclick = f6;
@@ -85,8 +104,20 @@ document.querySelector('.b-6').onclick = f6;
 // Вывод в out-7
 
 let ar7 = ['china', 'india', 'brazil', 'japan', 'egypt'];
+let out7 = document.querySelector('.out-7');
 
 function f7() {
+  ar7[5] = 'italy';
+  ar7[6] = 'turkey';
+  ar7[7] = 'vietnam';
+
+  let out = '';
+
+  for (let i = 0; i <= ar7.length - 1; i++) {
+    out += ar7[i] + ' ';
+  }
+
+  out7.innerHTML = out;
 }
 
 document.querySelector('.b-7').onclick = f7;
@@ -99,8 +130,22 @@ document.querySelector('.b-7').onclick = f7;
 // Вывод в out-8
 
 let ar8 = [];
+let out8 = document.querySelector('.out-8');
+let out81 = document.querySelector('.out-8-1');
 
 function f8() {
+  ar8[3] = '3.14';
+  ar8[4] = '17';
+  ar8[6] = '5';
+
+  let out = '';
+
+  for (let i = 0; i <= ar8.length - 1; i++) {
+    out += ar8[i] + '-';
+  }
+
+  out8.innerHTML = out;
+  out81.innerHTML = ar8.length;
 }
 
 document.querySelector('.b-8').onclick = f8;
@@ -112,8 +157,10 @@ document.querySelector('.b-8').onclick = f8;
 // Вывод в out-9
 
 let ar9 = [100, 200, 300, 400, 700, 121];
+let out9 = document.querySelector('.out-9');
 
 function f9() {
+  out9.innerHTML = ar9[ar9.length - 1];
 }
 
 document.querySelector('.b-9').onclick = f9;
@@ -125,12 +172,13 @@ document.querySelector('.b-9').onclick = f9;
 // Вывод в out-10
 
 let ar10 = [100, 200, 300, 400, 700, 121];
+let out10 = document.querySelector('.out-10');
 
 function f10() {
+  out10.innerHTML = ar10[1] + ar10[ar10.length - 1];
 }
 
 document.querySelector('.b-10').onclick = f10;
-
 
 // Task 11
 // Напишите функцию, которая меняет местами второй (индекс 2) и четвертый (индекс 4) элемент массива ar11 и выводит его в out-11. Разделитель - пробел.
@@ -140,13 +188,24 @@ document.querySelector('.b-10').onclick = f10;
 // Вывод в out-11
 
 let ar11 = [2, 3, 4, 5, 6, 7];
+let out11 = document.querySelector('.out-11');
 
 function f11() {
+  let s = 0;
+  s = ar11[2];
+  ar11[2] = ar11[4];
+  ar11[4] = s;
 
+  let out = '';
+
+  for (let i = 0; i <= ar11.length - 1; i++) {
+    out += ar11[i] + ' ';
+  }
+
+  out11.innerHTML = out;
 }
 
 document.querySelector('.b-11').onclick = f11;
-
 
 // Task 12
 // Напишите функцию f12, которая меняет местами нулевой и последний элемент массива ar12 и выводит его в out-12. Разделитель - пробел. Последний элемент вычислять через длину массива.
@@ -155,9 +214,21 @@ document.querySelector('.b-11').onclick = f11;
 // Вывод в out-12
 
 let ar12 = ['test', 'west', 'list', 'class', 'best'];
+let out12 = document.querySelector('.out-12');
 
 function f12() {
+  let s = '';
+  s = ar12[0];
+  ar12[0] = ar12[ar12.length - 1];
+  ar12[ar12.length - 1] = s;
 
+  let out = '';
+
+  for (let i = 0; i <= ar12.length - 1; i++) {
+    out += ar12[i] + ' ';
+  }
+
+  out12.innerHTML = out;
 }
 
 document.querySelector('.b-12').onclick = f12;
@@ -170,15 +241,20 @@ document.querySelector('.b-12').onclick = f12;
 // Тест допустим массив [1,2,3] вывод:
 //  0 1 1 2 2 3
 
-
 let ar13 = ['test', 'west', 'list', 'class', 'best'];
+let out13 = document.querySelector('.out-13');
 
 function f13() {
+  let out = '';
 
+  for (let i = 0; i <= ar13.length - 1; i++) {
+    out += i + ' ' + ar13[i] + ' ';
+  }
+
+  out13.innerHTML = out;
 }
 
 document.querySelector('.b-13').onclick = f13;
-
 
 // Task 14
 // Используя цикл выведите на страницу массив ar14 в обратном порядке. Разделитель - пробел.
@@ -187,9 +263,16 @@ document.querySelector('.b-13').onclick = f13;
 // Вывод в out-14
 
 let ar14 = [1, 2, 3, 'hello', 66];
+let out14 = document.querySelector('.out-14');
 
 function f14() {
+  let out = '';
 
+  for (let i = ar14.length - 1; i >= 0; i--) {
+    out += ar14[i] + ' ';
+  }
+
+  out14.innerHTML = out;
 }
 
 document.querySelector('.b-14').onclick = f14;
@@ -201,9 +284,18 @@ document.querySelector('.b-14').onclick = f14;
 // Вывод в out-15
 
 let ar15 = [0, 2, 5, -4, 6, 22, -9, -12, 8, 12, 13, 78];
+let out15 = document.querySelector('.out-15');
 
 function f15() {
+  let out = '';
 
+  for (let i = 0; i <= ar15.length - 1; i++) {
+    if (ar15[i] > 0) {
+      out += ar15[i] + ' ';
+    }
+  }
+
+  out15.innerHTML = out;
 }
 
 document.querySelector('.b-15').onclick = f15;
@@ -217,9 +309,29 @@ document.querySelector('.b-15').onclick = f15;
 let ar16 = [0, 2, 5, -4, 6, 22, -9, -12, 8, 12, 13, 78];
 let ar16_odd = [];
 let ar16_even = [];
+let out16_odd = document.querySelector('.out-16-odd');
+let out16_even = document.querySelector('.out-16-even');
 
 function f16() {
+  let out_odd = '';
+  let out_even = '';
+  let j = 0;
+  let k = 0;
 
+  for (let i = 0; i <= ar16.length - 1; i++) {
+    if (ar16[i] % 2 === 0) {
+      ar16_even[j] = ar16[i];
+      j++;
+      out_even += ar16[i] + ' ';
+    } else {
+      ar16_odd[k] = ar16[i];
+      k++;
+      out_odd += ar16[i] + ' ';
+    }
+  }
+
+  out16_odd.innerHTML = out_odd;
+  out16_even.innerHTML = out_even;
 }
 
 document.querySelector('.b-16').onclick = f16;
@@ -231,13 +343,21 @@ document.querySelector('.b-16').onclick = f16;
 // Вывод в out-17
 
 let ar17 = [3, 0, 2, 6, 0, 1, 3, 1, 9, 0, 2, 0];
+let out17 = document.querySelector('.out-17');
 
 function f17() {
+  let count = 0;
 
+  for (i = 0; i <= ar17.length - 1; i++) {
+    if (ar17[i] > 3) {
+      count++;
+    }
+  }
+
+  out17.innerHTML = count;
 }
 
 document.querySelector('.b-17').onclick = f17;
-
 
 // Task 18
 // Используя цикл выведите в .out-18 максимальный элемент массива ar18.
@@ -246,13 +366,23 @@ document.querySelector('.b-17').onclick = f17;
 // Вывод в out-18
 
 let ar18 = [15, 24, 13, 78, 21, 4, 45, 67];
+let out18 = document.querySelector('.out-18');
 
 function f18() {
+  let max = ar18[0];
 
+  for (i = 1; i <= ar18.length - 1; i++) {
+    if (max > ar18[i]) {
+      max = max;
+    } else {
+      max = ar18[i];
+    }
+  }
+
+  out18.innerHTML = max;
 }
 
 document.querySelector('.b-18').onclick = f18;
-
 
 // Task 19
 // Выведите в .out-19 индекс минимального элемента в массиве ar19
@@ -261,9 +391,22 @@ document.querySelector('.b-18').onclick = f18;
 // Вывод в out-19
 
 let ar19 = [15, 424, 313, 78, 241, 4, 45, 67];
+let out19 = document.querySelector('.out-19');
 
 function f19() {
+  let min = ar19[0];
+  let id = 0;
 
+  for (i = 1; i <= ar19.length - 1; i++) {
+    if (min < ar19[i]) {
+      min = min;
+    } else {
+      min = ar19[i];
+      id = i;
+    }
+  }
+
+  out19.innerHTML = id;
 }
 
 document.querySelector('.b-19').onclick = f19;
@@ -275,9 +418,16 @@ document.querySelector('.b-19').onclick = f19;
 // Вывод в out-20
 
 let ar20 = [4, 5, 6, 7, 8, 9, 10];
+let out20 = document.querySelector('.out-20');
 
 function f20() {
+  let sum = 0;
 
+  for (i = 0; i <= ar20.length - 1; i++) {
+    sum += ar20[i];
+  }
+
+  out20.innerHTML = sum;
 }
 
 document.querySelector('.b-20').onclick = f20;
